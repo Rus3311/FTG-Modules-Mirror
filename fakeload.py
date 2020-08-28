07184a2bebd4d4ac7c304b6dc716bb767e73dc33
Rus3311 <70165313+Rus3311@users.noreply.github.com>
2020-08-28 10:30:36 +0500

    Sentor.ry

--- fakeload.py
+++ fakeload.py
@@ -5,99 +5,99 @@ from time import sleep
 from userbot.events import register
 
 
-@register(outgoing=True, pattern='^.fl(?: |$)(.*)')
+@register(outgoing=True, pattern='^.senator(?: |$)(.*)')
 async def typewriter(typew):
     await typew.edit("`start loading...`")
     sleep(1)
     await typew.edit("0%")
     number = 1
-    await typew.edit(str(number) + "%   ▎")
+    await typew.edit(str(number) + "%   Я")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ▍")
+    sleep(1)
+    await typew.edit(str(number) + "%   хочу")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ▌")
+    sleep(1)
+    await typew.edit(str(number) + "%   тебе")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ▊")
+    sleep(1)
+    await typew.edit(str(number) + "%   сказать")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ▉")
+    sleep(1)
+    await typew.edit(str(number) + "%   что")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   █")
+    sleep(1)
+    await typew.edit(str(number) + "%   ты")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   █▎")
+    sleep(1)
+    await typew.edit(str(number) + "%   самая")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   █▍")
+    sleep(1)
+    await typew.edit(str(number) + "%   лучшая")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   █▌")
+    sleep(1)
+    await typew.edit(str(number) + "%   ты")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   █▊")
+    sleep(1)
+    await typew.edit(str(number) + "%   самая")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   █▉")
+    sleep(1)
+    await typew.edit(str(number) + "%   прекрасная")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ██")
+    sleep(1)
+    await typew.edit(str(number) + "%   доборая")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ██▎")
+    sleep(1)
+    await typew.edit(str(number) + "%   хорошая")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ██▍")
+    sleep(1)
+    await typew.edit(str(number) + "%   красивая")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ██▌")
+    sleep(1)
+    await typew.edit(str(number) + "%   улыбнись")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ██▊")
+    sleep(1)
+    await typew.edit(str(number) + "%   ведь")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ██▉")
+    sleep(1)
+    await typew.edit(str(number) + "%   тебе идет")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ███")
+    sleep(1)
+    await typew.edit(str(number) + "%   улыбка")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ███▎")
+    sleep(1)
+    await typew.edit(str(number) + "%   и")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ███▍")
+    sleep(1)
+    await typew.edit(str(number) + "%   кто-бы")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ███▌")
+    sleep(1)
+    await typew.edit(str(number) + "%   чтобы")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ███▊")
+    sleep(1)
+    await typew.edit(str(number) + "%   не говорил")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ███▉")
+    sleep(1)
+    await typew.edit(str(number) + "%   знай")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ████")
+    sleep(1)
+    await typew.edit(str(number) + "%   ты самая")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ████▎")
+    sleep(1)
+    await typew.edit(str(number) + "%   прекрасная")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ████▍")
+    sleep(1)
+    await typew.edit(str(number) + "%   в этом мире")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ████▌")
+    sleep(1)
+    await typew.edit(str(number) + "%   🖤")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ████▊")
+    sleep(1)
+    await typew.edit(str(number) + "%   С ЛЮБОВЬ")
     number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ████▉")
+    sleep(1)
+    await typew.edit(str(number) + "%   senator")
     number = number + 1
-    sleep(0.03)
+    sleep(1)
     await typew.edit(str(number) + "%   █████")
     number = number + 1
     sleep(0.03)

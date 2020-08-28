bb72fe112308036d07af128587b7f245628173ff
Rus3311 <70165313+Rus3311@users.noreply.github.com>
2020-08-28 10:49:18 +0500

    Senator.ry

--- fakeload.py
+++ fakeload.py
@@ -10,87 +10,59 @@ async def typewriter(typew):
     await typew.edit("`ПОЖАЛУЙСТА ПРОЧТИ ДО КОНЦА🖤`")
     sleep(1)
     await typew.edit("...")
-    number = 1
     await typew.edit(str(number) + "   Я")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   хочу")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   тебе")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   сказать")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   что")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   ты")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   самая")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   лучшая")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   ты")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   самая")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   прекрасная")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   доборая")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   хорошая")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   красивая")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   улыбнись")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   ведь")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   тебе идет")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   улыбка")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   и")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   кто-бы")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   чтобы")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   не говорил")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   знай")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "    ты самая")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   прекрасная")
-    number = number + 1
     sleep(1)
     await typew.edit(str(number) + "   в этом мире")
-    number = number + 1
     sleep(1)
-    await typew.edit(str(number) + "   🖤")
-    number = number + 1
+    await typew.edit(str(number) + "   ♥️😘❤️")
     sleep(1)
     await typew.edit(str(number) + "   С ЛЮБОВЬ")
     
